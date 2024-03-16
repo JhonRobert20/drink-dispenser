565362f879bf5a2605277c9af794e57015f4e59e
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-16 21:19:50 +0100

    feat: transaction entity

---
+++ domain/entities/transaction.py
@@ -0,0 +1,40 @@
+from domain.entities.product import Product
+from domain.constants import (
+    TransactionStatus,
+    PRODUCT_ENTITY_REQUIRED_ERROR,
+    TRANSACTION_STATUS_TYPE_ERROR,
+    TRANSACTION_FLOAT_TYPE_ERROR
+)
+class Transaction:
+    def __init__(self, product: Product, paid_amount: float, change_given: float, status: TransactionStatus):
+        if not isinstance(product, Product):
+            raise ValueError(PRODUCT_ENTITY_REQUIRED_ERROR)
+
+        if not isinstance(status, TransactionStatus):
+            raise ValueError(TRANSACTION_STATUS_TYPE_ERROR)
+
+        if not isinstance(paid_amount, float) or not isinstance(change_given, float):
+            raise ValueError(TRANSACTION_FLOAT_TYPE_ERROR)
+
+        self.product = product
+        self.paid_amount = paid_amount
+        self.change_given = change_given
+        self.status = status
+
+    def __str__(self):
+        return f"Transaction: {self.product} with status: {self.status}"
+
+    def mark_as_completed(self):
+        self.status = TransactionStatus.COMPLETED
+
+    def mark_as_pending(self):
+        self.status = TransactionStatus.PENDING
+
+    def mark_as_error(self):
+        self.status = TransactionStatus.ERROR
+
+    def mark_as_cancelled(self):
+        self.status = TransactionStatus.CANCELLED
+
+    def mark_as_refunded(self):
+        self.status = TransactionStatus.REFUNDED
