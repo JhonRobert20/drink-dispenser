d47149e1d19ee01df54c0afe896c7dc6949eb1c4
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-16 21:19:21 +0100

    feat: new constants for transaction entity

--- domain/constants.py
+++ domain/constants.py
@@ -1,1 +1,13 @@
+from enum import Enum
+
 EXPIRATION_DATE_FORMAT_ERROR = "expiration date must be a datetime.date instance"
+PRODUCT_ENTITY_REQUIRED_ERROR = "product must be a Product instance"
+TRANSACTION_STATUS_TYPE_ERROR = "status must be a TransactionStatus instance"
+TRANSACTION_FLOAT_TYPE_ERROR = "paid amount and change given must be float instances"
+
+class TransactionStatus(Enum):
+    COMPLETED = "completed"
+    PENDING = "pending"
+    ERROR = "error"
+    CANCELLED = "cancelled"
+    REFUNDED = "refunded"
