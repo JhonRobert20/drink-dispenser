7a25eba6ab07fbb19422cdfe58244cdd0f8dfe46
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-19 21:54:09 +0100

    refactor: slots now is a dictionary

--- src/domain/entities/vending_machine.py
+++ src/domain/entities/vending_machine.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import Dict, List, Optional
 
 from src.domain.constants import (
+    COIN_ENTITY_REQUIRED_ERROR,
     INVALID_COIN_ERROR,
+    SLOT_ENTITY_REQUIRED_ERROR,
     VENDING_MACHINE_ADD_PRODUCT_ERROR,
     TransactionStatus,
 )
@@ -18,43 +20,51 @@ class ProductSlot:
 
 
 class VendingMachine:
-    def __init__(self, slots: List[ProductSlot], coins: List[Coin]):
+    def __init__(self, slots: Dict[str, ProductSlot], coins: List[Coin]):
+        if not isinstance(slots, dict):
+            raise ValueError(SLOT_ENTITY_REQUIRED_ERROR)
+        if not isinstance(coins, list):
+            raise ValueError(COIN_ENTITY_REQUIRED_ERROR)
+
         self.slots = slots
         self.coins = coins
         self.coins_actual_transaction = []
 
     def add_product_to_slot(self, product: Product, slot_code: str):
-        slot = self.get_slot_by_code(slot_code)
+        slot: ProductSlot = self.get_slot_by_code(slot_code)
         if slot:
             if slot.products.get_without_consume().bar_code != product.bar_code:
                 raise ValueError(VENDING_MACHINE_ADD_PRODUCT_ERROR)
             slot.products.put(product)
+            return slot
 
         else:
             new_products_queue = PeekableProductsQueue([product])
-            self.slots.append(ProductSlot(new_products_queue, slot_code))
+            self.slots[slot_code] = ProductSlot(new_products_queue, slot_code)
+            return self.slots[slot_code]
 
-    def product_is_valid(self, slot_index: int):
-        product = self.slots[slot_index].products.get_without_consume()
-        if product:
-            return product.is_valid()
-        return False
+    def product_is_valid(self, slot_code: str):
+        slot = self.get_slot_by_code(slot_code)
+        if not slot:
+            return False
 
-    def check_can_expend(self, slot_index: int):
-        if self.product_is_valid(slot_index):
+        product = slot.products.get_without_consume()
+        return product and product.is_valid()
+
+    def check_can_expend(self, slot_code: str):
+        if self.product_is_valid(slot_code):
+            slot = self.get_slot_by_code(slot_code)
             coins_value = round(
                 sum([coin.denomination for coin in self.coins_actual_transaction]), 2
             )
-            if (
-                coins_value
-                >= self.slots[slot_index].products.get_without_consume().price
-            ):
+            if coins_value >= slot.products.get_without_consume().price:
                 return True
         return False
 
-    def consume_product_item(self, slot_index: int):
-        if self.check_can_expend(slot_index):
-            product_to_expend = self.slots[slot_index].products.get_if_exists()
+    def consume_product_item(self, slot_code: str):
+        if self.check_can_expend(slot_code):
+            slot = self.get_slot_by_code(slot_code)
+            product_to_expend = slot.products.get_if_exists()
             paid_amount = sum(
                 [coin.denomination for coin in self.coins_actual_transaction]
             )
@@ -80,41 +90,23 @@ class VendingMachine:
         self.coins_actual_transaction = []
 
     def add_coin_to_transaction(self, coin: Coin):
-        self.coins.append(coin)
-        self.coins_actual_transaction.append(coin)
         if not coin.is_valid():
-            self.coins_actual_transaction.pop()
-            self.coins.pop()
             raise ValueError(INVALID_COIN_ERROR)
 
-    def get_existing_slot_index(self, slot_code: str):
-        for slot in self.slots:
-            if slot.code == slot_code:
-                return self.slots.index(slot)
+        self.coins.append(coin)
+        self.coins_actual_transaction.append(coin)
+
+    def get_slot_by_code(self, slot_code: str) -> Optional[ProductSlot]:
+        if slot_code in self.slots:
+            return self.slots[slot_code]
         return None
 
-    def get_slot_by_code(self, slot_code: str):
-        for slot in self.slots:
-            if slot.code == slot_code:
-                return slot
+    def remove_slot(self, slot_code: str) -> Optional[ProductSlot]:
+        if slot_code in self.slots:
+            return self.slots.pop(slot_code)
         return None
 
-    def remove_slot(self, slot_code: str):
-        for slot in self.slots:
-            if slot.code == slot_code:
-                self.slots.remove(slot)
-
-    def check_stock_by_code(self, slot_code: str):
-        for slot in self.slots:
-            if slot.code == slot_code:
-                return slot.products.qsize()
+    def check_stock_by_code(self, slot_code: str) -> int:
+        if slot_code in self.slots:
+            return self.slots[slot_code].products.qsize()
         return 0
-
-    def check_stock_by_index(self, index: int):
-        return self.slots[index].products.qsize()
-
-    def check_all_stock(self):
-        return {
-            slot.code: (slot.products.qsize(), slot.products.get_name_without_consume())
-            for slot in self.slots
-        }
