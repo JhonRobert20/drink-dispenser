372ac1afcf437d34f3ab295f56469f7d2909c2a2
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-20 00:07:58 +0100

    fix: iso format to date

--- src/infrastructure/utils.py
+++ src/infrastructure/utils.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from src.domain.entities.product import Product
 from src.domain.utils import PeekableProductsQueue
 
 
 def document_to_product(document) -> Product:
+    expiration_date = datetime.strptime(document["expiration_date"], "%Y-%m-%d").date()
     return Product(
         name=document["name"],
         bar_code=document["bar_code"],
-        expiration_date=document["expiration_date"],
+        expiration_date=expiration_date,
         price=document["price"],
     )
 
