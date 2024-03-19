1d05ed75d70c83569fb11eb9219b235cf619c171
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-20 00:05:29 +0100

    refactor: add to dict property

--- src/infrastructure/persistence/models/product.py
+++ src/infrastructure/persistence/models/product.py
@@ -5,4 +5,13 @@ class ProductModel:
     def __init__(self, product: Product):
         self.name = product.name
         self.bar_code = product.bar_code
-        self.expiration_date = product.expiration_date
+        self.expiration_date = product.expiration_date.isoformat()
+        self.price = product.price
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "bar_code": self.bar_code,
+            "expiration_date": self.expiration_date,
+            "price": self.price,
+        }
--- src/infrastructure/persistence/models/product_slot.py
+++ src/infrastructure/persistence/models/product_slot.py
@@ -8,3 +8,9 @@ class ProductSlotModel:
             ProductModel(product) for product in product_slot.products.queue
         ]
         self.code = product_slot.code
+
+    def to_dict(self):
+        return {
+            "products": [product.to_dict() for product in self.products],
+            "code": self.code,
+        }
