12d7913a32b53121d05738f7e82f8bcddffa828c
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-24 17:27:58 +0100

    feat: add coin command

---
+++ src/application/commands/add_coin.py
@@ -0,0 +1,21 @@
+from logging import Logger
+from typing import Optional
+
+from src.domain.constants import INVALID_COIN_ERROR
+from src.domain.entities.product import Product
+from src.domain.entities.vending_machine import VendingMachine
+from src.domain.value_objects.coin import Coin
+
+
+class AddCoinCommand:
+    def __init__(self, vending_machine: VendingMachine, logger: Logger):
+        self.vending_machine = vending_machine
+        self.logger = logger
+
+    def execute(self, coin_denomination: float) -> Optional[Product]:
+        try:
+            coin = Coin(coin_denomination, "EUR")
+            return self.vending_machine.add_coin_to_transaction(coin)
+        except ValueError:
+            self.logger.error(INVALID_COIN_ERROR)
+            return None
