ac17b905d3c8268f2db3535388d2212830b05258
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-26 13:08:39 +0100

    feat: add mosquitto

--- src/infrastructure/mqtt/mqtt_client.py
+++ src/infrastructure/mqtt/mqtt_client.py
@@ -1,3 +1,5 @@
+import random
+
 import paho.mqtt.client as mqtt
 from paho.mqtt.enums import CallbackAPIVersion
 from src.application.handlers.add_product import AddProductHandler
@@ -29,30 +31,43 @@ def configure_handlers(vending_machine, event_publisher=None):
 
 
 def start_and_configure_mqtt_client(
-    broker="mqtt.example.com", port=1883, vending_machine=None
+    logger,
+    broker="mqtt.example.com",
+    port=1883,
+    vending_machine=None,
 ):
-    mqtt_client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
+    client_id = f"python-mqtt-{random.randint(0, 1000)}"
+    mqtt_client = mqtt.Client(
+        client_id=client_id, callback_api_version=CallbackAPIVersion.VERSION2
+    )
+    mqtt_client.username_pw_set("user", "password")
 
     event_publisher = MqttEventPublisher(mqtt_client)
     topic_handlers = configure_handlers(vending_machine, event_publisher)
 
-    def on_connect(client, userdata, flags, rc):
-        print(f"Connected with result code {rc}")
-        for topic in topic_handlers.keys():
-            client.subscribe(topic)
-
-    def on_message(client, userdata, msg):
-        handler = topic_handlers.get(msg.topic)
-        if handler:
-            handler(msg.payload.decode("utf-8"))
+    def on_connect(client, userdata, flags, rc, *args, **kwargs):
+        if rc == 0:
+            logger.info("Connected to broker")
         else:
-            print(f"No handler for topic {msg.topic}")
+            logger.error(f"Connection failed with code {rc}")
+
+    def subscribe_topics(client):
+        def on_message(client, userdata, msg):
+            handler = topic_handlers.get(msg.topic)
+            logger.info(
+                f"Received message {msg.payload.decode('utf-8')} on topic {msg.topic}"
+            )
+            if handler:
+                handler(msg.payload.decode("utf-8"))
+            else:
+                logger.warning(f"No handler for topic {msg.topic}")
+
+        for topic in topic_handlers.keys():
+            mqtt_client.subscribe(topic)
+            mqtt_client.on_message = on_message
 
     mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
-
-    mqtt_client.connect(broker, port, 60)
+    mqtt_client.connect(host=broker, port=port, keepalive=60)
+    subscribe_topics(mqtt_client)
     mqtt_client.loop_start()
-
-
-start_and_configure_mqtt_client(vending_machine=vending_machine_instance)
+    return mqtt_client
