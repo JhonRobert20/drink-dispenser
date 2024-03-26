8e96b700c7f1435772ff053fd8e253f44ddbf177
JhonRobert20 <jhonbara51214@gmail.com>
2024-03-26 20:02:03 +0100

    feat: get coins from document

--- src/infrastructure/utils.py
+++ src/infrastructure/utils.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from typing import List
 
 from src.domain.entities.product import Product
 from src.domain.utils import PeekableProductsQueue
+from src.domain.value_objects.coin import Coin
 
 
 def document_to_product(document) -> Product:
@@ -14,6 +16,13 @@ def document_to_product(document) -> Product:
     )
 
 
+def document_to_coin(document) -> Coin:
+    return Coin(
+        denomination=document["denomination"],
+        currency=document["currency"],
+    )
+
+
 def products_documents_to_product_slot(product_documents) -> PeekableProductsQueue:
     return PeekableProductsQueue(
         [
@@ -21,3 +30,10 @@ def products_documents_to_product_slot(product_documents) -> PeekableProductsQue
             for product_document in product_documents
         ]
     )
+
+
+def transaction_document_to_coins(transaction_document) -> List[Coin]:
+    return [
+        document_to_coin(coin_document)
+        for coin_document in transaction_document["coins"]
+    ]
